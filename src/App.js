a54d26b795239205a04a6d2f5d6c00f98133f594
fanmu <fanmucc@yeah.net>
2020-08-27 23:03:58 +0800

    添加事件

--- src/App.js
+++ src/App.js
@@ -6,25 +6,41 @@ class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            value: '',
+            value: '123',
             list: []
         }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleClick = this.handleClick.bind(this)
     }
     render() { 
         return ( 
             <div className="content">
                 <div className="anmo">
-                    <div style={{'display': 'flex'}}><Input placeholder="需要什么服务" /> <Button type="primary" style={{'margin-left':'10px'}}>增加服务</Button></div>
+                    <div style={{'display': 'flex'}}>
+                        <Input placeholder="需要什么服务" value={this.state.value} onChange={this.handleChange}/> 
+                        <Button type="primary" style={{'marginLeft':'10px'}} onClick={this.handleClick}>增加服务</Button>
+                    </div>
                     <ul>
-                        <li>推油</li>
-                        <li>按摩</li>
+                        {
+                            this.state.list.map((item, index) => {
+                                return <li key={index}>{item}</li>
+                            })
+                        }
                     </ul>
                 </div>
             </div>
          );
     }
-    hanldrenClick () {
-        alert(1)
+    handleChange (e) {
+        console.log(e.target.value)
+        this.setState({
+            value: e.target.value
+        })
+    }
+    handleClick () {
+        this.setState({
+            list: [...this.state.list, this.state.value]
+        })
     }
 }
  
